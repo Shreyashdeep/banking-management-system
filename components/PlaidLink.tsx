db1651d5c57b46dd7be79cc832228fdc98f6c724
Shreyashdeep <shreyashdeep20@gmail.com>
2024-09-12 00:37:01 +0530

    implement add bank and transaction history

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -4,6 +4,7 @@ import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink} from 'react-plaid-l
 import { StyledString } from 'next/dist/build/swc';
 import { useRouter } from 'next/navigation';
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
+import Image from 'next/image';
 
 const PlaidLink = ({user,variant}: PlaidLinkProps) => {
   const router= useRouter();
@@ -41,12 +42,25 @@ const PlaidLink = ({user,variant}: PlaidLinkProps) => {
             Connect bank
         </Button>
      ):variant=='ghost' ? (
-        <Button>
+        <Button onClick={() => open()} variant="ghost" className='plaidlink-ghost'>
+           <Image
+            src="/icons/connect-bank.svg"
+            alt="connect bank"
+            width={24}
+            height={24}
+          />
+          <p className='hiddenl text-[16px] font-semibold text-black-2 xl:block'>Connect Bank</p>
             connect bank
         </Button>
      ):(
-        <Button>
-            connect bank
+        <Button onClick={() => open()} className='plaidlink-default'>
+          <Image
+            src="/icons/connect-bank.svg"
+            alt="connect bank"
+            width={24}
+            height={24}
+          />
+          <p className='text-[16px] font-semibold text-black-2'>Connect Bank</p>
         </Button>
      )}
     </>

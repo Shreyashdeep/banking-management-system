653fdfae1ac0126f821a56aa9697f1abb8f94bcb
Shreyash_deep <shreyashdeep20@gmail.com>
2024-09-15 10:54:05 +0530

    update rightsidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import React from 'react'
 import BankCard from './BankCard'
 import { countTransactionCategories } from '@/lib/utils'
-import Category from './Category'
 
 export const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
     const categories: CategoryCount[]=
@@ -70,9 +69,7 @@ export const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => 
             <div className='mt-10 flex flex-1 flex-col'>
                 <h2 className='header-2'>Top Categories</h2>
                 <div className='space-y-5'>
-                    {categories.map((category, index)=>(
-                        <Category key={category.name} category={category}/>
-                    ))}
+                    
 
                 </div>
 
